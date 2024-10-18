0b7068231d5b8868c0e637e3613ecb730de73c30
Radwa Khaled <131785821+radwakhaled204@users.noreply.github.com>
2024-10-18 17:46:15 +0300

    Update 0-uniq_users.sql

--- 0x00-MySQL_Advanced /0-uniq_users.sql
+++ 0x00-MySQL_Advanced /0-uniq_users.sql
@@ -1,7 +1,8 @@
--- lets create some users shall we?
+-- Creates a table with unique users email address.
 
-CREATE TABLE IF NOT EXISTS users(
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
-);
+CREATE TABLE IF NOT EXISTS
+    users (
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        email VARCHAR (255) NOT NULL UNIQUE,
+        name VARCHAR (255)
+    );
